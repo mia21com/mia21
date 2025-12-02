bc683442713d189d2374ca5c3c45d30c3a0cf705
Nataliia <nataliia@earkick.com>
2025-12-02 15:21:23 +0200

    update swift package.

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "Mia21",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .watchOS(.v8),
+        .tvOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "Mia21",
+            targets: ["Mia21"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Mia21",
+            dependencies: [],
+            path: "ios/Sources/Mia21"
+        ),
+        .testTarget(
+            name: "Mia21Tests",
+            dependencies: ["Mia21"],
+            path: "ios/Tests/Mia21Tests"
+        ),
+    ]
+)
+
